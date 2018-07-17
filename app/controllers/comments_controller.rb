2260d03c9cf61ab87e86936174448bdba8daa7a2
Keiko Yoshida <kitchen8nn@gmail.com>
2018-07-17 11:07:41 +0900

    fix comments#create controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[:id])
-    @comment = @post.comment.create(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
 
     redirect_to post_path(@post)
   end

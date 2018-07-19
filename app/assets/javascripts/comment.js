06d12bd2e1c9d6ee67122c8641710e6ddca21311
Keiko Yoshida <kitchen8nn@gmail.com>
2018-07-19 15:15:35 +0900

    append new comment html with js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -12,9 +12,29 @@
 // });
 
 $(document).on("ajax:success", "#new-comment", function(e) {
-  console.log(e.detail[0]);
-
+// create new comment html
+  var html = buildHTML(e.detail[0].comment);
+  $('.comments').append(html);
+  $('.commentform_name').val('');
+  $('.commentform_body').val('');
+// scroll to the page bottom
+  $('html, body').animate({
+      scrollTop: $(document).height()
+    },1500);
+  // $('.comments').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  // flash();
 });
 $(document).on("ajax:error", "#new-comment", function(e) {
   console.log(e.detail[2]);
 });
+
+
+function buildHTML(comment){
+  var html = '<h3 class ="font-weight-bold mt-4">\n'
+           + comment.name
+           + '</h3>\n'
+           + '<div class ="mt-2">'
+           + comment.body
+           + '</div>\n';
+  return html;
+}

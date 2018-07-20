4381a9e3d16c80d8f7e6f42d8721f7b1ce1b314e
Keiko Yoshida <kitchen8nn@gmail.com>
2018-07-20 15:59:45 +0900

    set path for pages#about function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
+  get '/about', to: 'pages#about'
+
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
   end
